5d5b8ac75e50e70cd921d2d2c89472272422af26
jonathan <Jmerol27@gmail.com>
2024-02-06 12:44:34 -0600

    primer submit
    creacion de aplicacion de react con consumo de api

---
+++ src/hooks/useBebidas.jsx
@@ -0,0 +1,10 @@
+import { useContext } from "react";
+import BebidasContext from "../context/bebidasProvider"; "../context/BebidasProvider";
+
+const useBebidas = () => {
+
+    return useContext(BebidasContext)
+
+}
+
+export default useBebidas
